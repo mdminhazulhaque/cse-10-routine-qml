778c327f985fc5aab51e885d67413bfb3b3dd230
MD Minhazul Haque <mdminhazulhaque@gmail.com>
2013-01-06 11:37:46 +0600

    Update defines.h
    Modified indent

--- defines.h
+++ defines.h
@@ -4,20 +4,18 @@
 #define APP_VERSION "1.0"
 
 // win32
-#define VER_FILEVERSION                             1,0,0,0
-#define VER_FILEVERSION_STR                 "1.0.0.0\0"
-
-#define VER_PRODUCTVERSION              1,0,0,0
+#define VER_FILEVERSION             1,0,0,0
+#define VER_FILEVERSION_STR         "1.0.0.0\0"
+#define VER_PRODUCTVERSION          1,0,0,0
 #define VER_PRODUCTVERSION_STR      "1.0\0"
-
-#define VER_COMPANYNAME_STR           "MD Minhazul Haque"
-#define VER_FILEDESCRIPTION_STR          "CSE 10 Routine - QML"
-#define VER_INTERNALNAME_STR           "cse-10-routine-qml.exe"
-#define VER_LEGALCOPYRIGHT_STR         "Copyright (c) 2012 MD Minhazul Haque"
+#define VER_COMPANYNAME_STR         "MD Minhazul Haque"
+#define VER_FILEDESCRIPTION_STR     "CSE 10 Routine - QML"
+#define VER_INTERNALNAME_STR        "cse-10-routine-qml.exe"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (c) 2012 MD Minhazul Haque"
 #define VER_LEGALTRADEMARKS1_STR    "This program comes with ABSOLUTELY NO WARRANTY"
 #define VER_LEGALTRADEMARKS2_STR    "This is free software, and you are welcome to redistribute it under certain conditions"
 #define VER_ORIGINALFILENAME_STR    "somewhereinblog-desktop-notifier.exe"
-#define VER_PRODUCTNAME_STR             "CSE 10 Routine - QML"
-#define VER_COMPANYDOMAIN_STR      "http://mdminhazulhaquebn.blogspot.com"
+#define VER_PRODUCTNAME_STR         "CSE 10 Routine - QML"
+#define VER_COMPANYDOMAIN_STR       "http://mdminhazulhaquebn.blogspot.com"
 
 #endif // DEFINES_H
